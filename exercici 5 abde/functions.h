b1eab8e61228148aeba26a1ac40ae619beb9cb4c
Abdelaziz <95416754+aabdee@users.noreply.github.com>
2022-01-26 10:45:15 +0100

    ...

---
+++ exercici 5 abde/functions.h
@@ -0,0 +1,14 @@
+/*
+ * functions.h
+ *
+ *  Created on: 26 ene 2022
+ *      Author: admin-pg
+ */
+
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#endif /* FUNCTIONS_H_ */
+
+
+int intercanvi(int, int);
